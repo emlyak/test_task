292f7751b29b9682e1cd6ce658e1b7a49607e377
Egor Lyakhovets <em@lyakhovets.com>
2024-09-12 15:16:08 +0700

    add header file

--- parser.h
+++ parser.h
@@ -2,6 +2,7 @@
 #define PARSER_H
 #include <string>
 #include <vector>
+#include <cmath>
 #include <complex>
 #include <fstream>
 
@@ -13,14 +14,17 @@ public:
 
     ~Parser() = default;
 
-    void read_file(const std::string file_name);
+    void read_file(const std::string& file_name);
 
     std::vector<double> freqs;
-    std::vector<std::complex<double>> data;
+    std::vector<double> data;
 
 
 private:
     std::string file_name;
+
+    void fit_to_window(const size_t& width, size_t& height);
+    void to_real_space();
 
 
 };
